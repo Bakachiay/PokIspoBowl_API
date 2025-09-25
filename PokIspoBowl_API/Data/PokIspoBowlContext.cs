c91e62a7fd79b28dbf70b4a1cba7495916773ac4
jeromefourdrain <jerome.fourdrain@gmail.com>
2025-09-25 15:57:31 +0200

    AJout des DbSet manquants dans le dbContext

--- PokIspoBowl_API/Data/PokIspoBowlContext.cs
+++ PokIspoBowl_API/Data/PokIspoBowlContext.cs
@@ -13,8 +13,17 @@ namespace PokIspoBowl_API.Data
             
         }
 
-        //TODO : finir les tables ici
-        public DbSet<Bowl> Bowls => Set<Bowl>();
-        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Bowl> Bowls { get; set; }
+        public DbSet<Dessert> Desserts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<Taste> Tastes { get; set; }
+        public DbSet<SelectedIngredient> SelectedIngredients { get; set; }
+
+
     }
 }
